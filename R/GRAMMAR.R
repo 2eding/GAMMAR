203b6b18e303436309e4aa4327b40b8b83348fa8
e2ding <e2ding@localhost.localdomain>
2018-12-03 06:14:44 +0900

    .

--- R/GRAMMAR.R
+++ R/GRAMMAR.R
@@ -73,7 +73,7 @@ run_grammar<- function(K, Y, X, VC, max_itr, num.parallel) {
     doParallel::registerDoParallel(cl)
     Sys.setenv("MC_CORES"=num.parallel)
     
-    foreach::foreach(i=1:Ng) foreach::`%dopar%` {
+    foreach::foreach(i=1:Ng) foreach::"%dopar%" {
       pval[i] = gamma(newY, X[, i], max_itr)
       fval[i] = getF(newY, X[, i], 1)
       cat(i, ". f =", fval[i], " p =", pval[i], "\n")
