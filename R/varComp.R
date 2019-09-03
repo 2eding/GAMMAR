17ec7ef0cfa64ae1d1aa2716e13bbd966a913c89
2eding <2eding@dongguk.edu>
2019-09-04 05:12:06 +0900

    final

--- R/varComp.R
+++ R/varComp.R
@@ -4,41 +4,66 @@
 #' 
 #' @param K is obtained from the Kinship function
 #' @param Y is the phenotype matrix, individual x phenotypes
-#' @param num.parallel Number of parallel processes or a predefined socket cluster
+#' @param X is the SNP matrix, individual x SNPs
 #' 
 #' @return Variance components Vg: genetic factor Ve: environment factor
 #' 
-#' @importFrom pbmcapply pbmclapply
-#' @importFrom regress regress
+#' @importFrom lmmlite eigen_rotation
+#' @importFrom lmmlite fitLMM
 #' 
 #' @examples 
-#'    X <- as.matrix(data.table::fread(Genotypesdata))
-#'    Y <- as.matrix(data.table::fread(Phenotypesdata))
-#'    K <- Kinship(X)
+#'    X = as.matrix(read.table(Genotypesdata))
+#'    Y = as.matrix(read.table(Phenotypesdata))
+#'    K = Kinship(X)
 #'    
-#'    VC <- varComp(Y, K, num.parallel)
+#'    VC = varComp(K, Y, X)
 #' @export
 
 
-varComp <- function(Y, K, num.parallel){
+varComp <- function(K, Y, X){
   ptm <- proc.time()
-
-  Y <- t(Y)
-  calvc <- function(Y, K, core){
-    list_Y <- as.list(as.data.frame(t(Y)))
-    length(list_Y)
+  K_origin <- K
+  
+  for (i in 1:dim(Y)[2]) {
+    K <- K_origin
+    print(i)
     
-    raw_vc <- pbmcapply::pbmclapply(list_Y, function(Y){
-      regress::regress(Y ~ 1, ~ K, pos=c(T, T), tol=1e-8)$sigma},
-      mc.cores = core
-      )
+    e = lmmlite::eigen_rotation(K, t(Y)[i,], use_cpp = T)
+    VC = lmmlite::fitLMM(
+      e$Kva,
+      e$y,
+      e$X,
+      reml = T,
+      use_cpp = T,
+      tol = 1e-6,
+      check_boundary = T
+    )
     
-    return(do.call(rbind, raw_vc))
+    write.table(
+      VC$sigmasq_g,
+      "Vg_temp.txt",
+      row.names = F,
+      col.names = F,
+      append = T,
+      quote = F,
+      sep = "\n"
+    )
+    write.table(
+      VC$sigmasq_e,
+      "Ve_temp.txt",
+      row.names = F,
+      col.names = F,
+      append = T,
+      quote = F,
+      sep = "\n"
+    )
   }
   
-  ncore <- num.parallel
-  vc <- calvc(Y, K, core = ncore)
-  write.table(vc, "./VC.txt", row.names = F, col.names = F, quote = F)
+  VCbind <- cbind(vg=as.matrix(read.table("Vg_temp.txt")), ve=as.matrix(read.table("Ve_temp.txt")))
+  file.remove("Vg_temp.txt")
+  file.remove("Ve_temp.txt")
+  write.table(VCbind, "VC.txt", row.names = F, col.names = F, quote = F)
+  vc <- as.matrix(read.table("VC.txt"))
   
   print(proc.time() - ptm)
   return(vc)
