a3fa71fbd5445a8f822a3a0f06b87c51fb065b79
2eding <2eding@dongguk.edu>
2019-10-26 12:19:12 +0900

    .

--- R/run_grammar.R
+++ R/run_grammar.R
@@ -111,7 +111,7 @@ run_grammar<- function(K, Y, X, VC, max_itr, num.parallel, outPath, outname) {
     
     for(i in 1:src_files_cnt){
       tempResult <- as.matrix(read.table(paste(src_dir, src_files[i], sep = ""),))
-      write.table(tempResult, paste(outPath, outname, sep = ""), row.names = F, col.names = F, quote = F, append = T)  
+      write.table(tempResult, paste(outPath, outname, sep = ""), sep = "\t", row.names = F, col.names = F, quote = F, append = T)  
     }
     
     tempread <- as.matrix(read.table(paste(outPath, outname, sep = "")))
